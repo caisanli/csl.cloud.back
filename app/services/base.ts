96a5864714c50aefc5719182d85c78b7196cfa19
caigx <caigx@zxjmail.com>
2020-11-16 17:39:31 +0800

    增加baseService

---
+++ app/services/base.ts
@@ -0,0 +1,30 @@
+import { DeleteResult, getRepository, Repository, UpdateResult } from "typeorm";
+
+export class BaseService<T> {
+    public repository: Repository<T>
+
+    constructor(entityClass: any) {
+        this.repository = getRepository(entityClass, 'mysql');
+    }
+
+    create(entity: T): Promise<T> {
+        const newEntity = this.repository.create(entity);
+        return this.repository.save(newEntity);
+    }
+
+    update(id: string | number, entity: T): Promise<UpdateResult> {
+        return this.repository.update(id, entity);
+    }
+
+    remove(id: string | number): Promise<DeleteResult> {
+        return this.repository.delete(id);
+    }
+
+    getById(id: string | number): Promise<T> {
+        return this.repository.findOne(id);
+    }
+
+    getAll(): Promise<T []> {
+        return this.repository.find();
+    }
+}
