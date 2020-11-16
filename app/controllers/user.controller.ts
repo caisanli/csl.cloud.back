d81c31c4dbab7bf5e27523de948f3a151a681c36
CaisanLi <18081007254@sina.cn>
2020-11-16 21:45:46 +0800

    增加字段验证

--- app/controllers/user.controller.ts
+++ app/controllers/user.controller.ts
@@ -12,6 +12,11 @@ export class UserController {
 
     @Post()
     async create(@Body() user: User) {
+        const errors = await UserController.validate(user);
+        if(errors.length) {
+
+            return ;
+        }
         const result = await this.userService.create(user);
         console.log('result', result);
         return '保存成功';
@@ -22,5 +27,29 @@ export class UserController {
         const result = await this.userService.getAll();
         console.log('result', result);
         return '获取成功';
+    }
+
+    static validate(user: User): Promise<any []> {
+        const { name, password, phone, email } = user;
+        const errors = [], 
+            pswReg = /^.*(?=.{6,})(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*? ]).*$/,
+            phoneReg = /^1[\d]{10}$/,
+            emailReg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+        if(name.length < 2 || name.length > 20) {
+            errors.push({prop: 'name', message: '用户名长度在2-20之间'})
+        }
+        if(password.length < 6) {
+            errors.push({prop: 'password', message: '密码长度不能小于6'})
+        }
+        if(!pswReg.test(password)) {
+            errors.push({prop: 'password', message: '密码至少包含1个大写字母，1个小写字母，1个数字，1个特殊字符'})
+        }
+        if(phone && !phoneReg.test(phone)) {
+            errors.push({prop: 'phone', message: '手机格式不正确'})
+        }
+        if(email && !emailReg.test(email)) {
+            errors.push({prop: 'email', message: '邮箱格式不正确'})
+        }
+        return Promise.resolve(errors);
     }
 }
