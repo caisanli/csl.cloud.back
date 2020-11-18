91b0920ccff7b8cd6d1bba639eb2b11ab598720f
caigx <caigx@zxjmail.com>
2020-11-18 17:59:50 +0800

    增加service

--- app/services/folder.service.ts
+++ app/services/folder.service.ts
@@ -1,4 +1,5 @@
 import { Folder } from "app/entities/mysql";
+import { Console } from "console";
 import { Service } from "typedi";
 import { BaseService } from "./base";
 
@@ -6,5 +7,19 @@ import { BaseService } from "./base";
 export class FolderService extends BaseService<Folder> {
     constructor() {
         super(Folder)
+    }
+
+    getFolderByUserAndParent(userId?: string, parentId?: string, name?: string): Promise<Folder []> {
+        const wheres:string [] = [];
+        if(userId)
+            wheres.push('group.userId = :userId');
+        if(parentId)
+            wheres.push('group.parentId = :parentId');
+        if(name)
+            wheres.push('group.name = :name');
+        console.log(wheres.join(' AND '));
+        return this.repository.createQueryBuilder()
+            .where(wheres.join(' AND '), { userId, parentId, name })
+            .getMany();
     }
 }
--- app/services/group.service.ts
+++ app/services/group.service.ts
@@ -1,8 +1,31 @@
-import { Group } from "app/entities/mysql";
+import { Group, GroupRole, User } from "app/entities/mysql";
 import { BaseService } from "./base";
 
 export class GroupService extends BaseService<Group> {
     constructor() {
         super(Group)
+    }
+
+    findByName(name: string): Promise<Group []> {
+        return this.repository.createQueryBuilder('group')
+            .where('group.name LIKE :name')
+            .setParameter('name', `%${ name }%`)
+            .getMany()
+    }
+
+    async findUsersById(id: string): Promise<User []> {
+        const { users } = await this.repository.createQueryBuilder('group')
+            .leftJoinAndSelect('group.users', 'user')
+            .where('group.id = :id', { id })
+            .getOne();
+        return Promise.resolve(users);
+    }
+
+    async findRolesById(id: string): Promise<GroupRole []> {
+        const { roles } = await this.repository.createQueryBuilder('group')
+            .leftJoinAndSelect('group.roles', 'role')
+            .where('group.id = :id', { id })
+            .getOne();
+        return Promise.resolve(roles);
     }
 }
