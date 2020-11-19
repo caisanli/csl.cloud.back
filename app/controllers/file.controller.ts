c91d4a244a5683569d689b80e3f27a351ee8dc25
CaisanLi <18081007254@sina.cn>
2020-11-19 22:25:58 +0800

    完善上传

--- app/controllers/file.controller.ts
+++ app/controllers/file.controller.ts
@@ -1,9 +1,9 @@
 import { FileChunkEntity } from "app/entities/mongodb";
-import { createFileHash } from "app/helpers";
+import { createFileHash, twoDecimal } from "app/helpers";
 import { fileUploadOptions, mergeFile } from "app/helpers/upload";
 import { UserAuthMiddleware } from "app/middlewares/userAuth";
 import { FileChunkService } from "app/services/file.chunk.service";
-import { Body, Get, JsonController, Param, Post, UploadedFile, UseBefore } from "routing-controllers";
+import { Body, Delete, Get, JsonController, Param, Post, UploadedFile, UseBefore } from "routing-controllers";
 
 @JsonController('/file')
 // @UseBefore(UserAuthMiddleware)
@@ -14,29 +14,53 @@ export class FileController {
         this.fileChunkService = new FileChunkService();
     }
 
+    /**
+     * 个人文件上传
+     * @param file 
+     * @param bodyFile 
+     */
     @Post("/upload")
-    async upload(@UploadedFile('file', { options: fileUploadOptions }) file: any, @Body() bodyFile: any) {
+    async upload(@UploadedFile('file', { options: fileUploadOptions }) file: any, @Body() bodyFile: FileChunkEntity) {
         const hashVal = createFileHash(bodyFile.name, bodyFile.size, bodyFile.modifyTime);
+        let data: {
+            process: number,
+            uploaded: boolean,
+        } = {
+            process: 0,
+            uploaded: false
+        };
         if(bodyFile.chunk >= bodyFile.chunks) { // 全部分片上传完成
             mergeFile(hashVal, bodyFile.name);
+            let newFileChunk = new FileChunkEntity();
+            newFileChunk.id = hashVal;
+            this.fileChunkService.removeByCondition(newFileChunk);
+            data.process = 1;
+            data.uploaded = true;
         } else { // 分片未上传完成
             const fileChunk = new FileChunkEntity();
-            fileChunk._id = hashVal;
             fileChunk.id = hashVal;
+            fileChunk._id = hashVal;
             fileChunk.name = bodyFile.name;
-            fileChunk.start = bodyFile.start;
-            fileChunk.end = bodyFile.end;
+            fileChunk.modifyTime = bodyFile.modifyTime;
             fileChunk.chunk = bodyFile.chunk;
             fileChunk.chunks = bodyFile.chunks;
             fileChunk.size = file.size;
+            data.process = twoDecimal(Number(bodyFile.chunk) / Number(bodyFile.chunks));
             await this.fileChunkService.create(fileChunk);
         }
-        return { message: '上传成功', data: {  }, code: 1 }
+        return { message: '上传成功', data: data, code: 1 }
     }
 
     @Get('/chunk/:id')
     async getFileChunkById(@Param('id') id: string) {
         const result = await this.fileChunkService.getById(id);
         return { message: '查询成功', data: result, code: 1 }
+    }
+
+    @Delete('/chunk')
+    async deleteFileChunkById() {
+        let newFileChunk = new FileChunkEntity();
+        newFileChunk.id = '421d0f38361a50d759ac0e9a0d87e2ba';
+        this.fileChunkService.removeByCondition(newFileChunk);
     }
 }
