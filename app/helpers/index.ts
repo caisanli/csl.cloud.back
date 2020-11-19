86ca173795771a3096d06bf80098b53a2de90f92
CaisanLi <18081007254@sina.cn>
2020-11-19 22:24:40 +0800

    增加公共方法

--- app/helpers/index.ts
+++ app/helpers/index.ts
@@ -1,5 +1,7 @@
 import md5 from "md5";
-
+/**
+ * 生成当前时间信息
+ */
 export function currentTime() {
     const dateInstance = new Date();
     let year = dateInstance.getFullYear();
@@ -14,12 +16,30 @@ export function currentTime() {
     let monthStr = month < 10 ? '0' + month : month + '';
     return { yearStr: year, dateStr, minutesStr, secondsStr, monthStr, ms: dateInstance.getMilliseconds() }
 }
-
+/**
+ * 生成重复名称
+ * @param name 
+ */
 export function createRepeatName(name: string) {
     let { yearStr, monthStr, dateStr, ms } = currentTime();
     return name + '_' + yearStr + monthStr + dateStr + '_' + ms
 }
 
+/**
+ * 根据文件信息生成hash
+ * @param name 
+ * @param size 
+ * @param modifyDate 
+ */
 export function createFileHash(name: string, size: string, modifyDate: string) {
     return md5(name + size + modifyDate);
+}
+
+/**
+ * 保留两位小数
+ * @param decimal 
+ */
+export function twoDecimal(decimal: number): number {
+    console.log('decimal：' + decimal)
+    return Math.round(decimal * 100) / 100;
 }
