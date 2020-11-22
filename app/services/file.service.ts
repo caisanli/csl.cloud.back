6b40b65ad77ced76414065a3aad30836a4a83f77
CaisanLi <18081007254@sina.cn>
2020-11-22 23:00:02 +0800

    增加查询

--- app/services/file.service.ts
+++ app/services/file.service.ts
@@ -1,9 +1,21 @@
 import { File } from "app/entities/mysql";
+import { ORDER } from "app/typings";
 import { Service } from "typedi";
 import { BaseService } from "./base";
 @Service()
 export class FilService extends BaseService<File> {
     constructor() {
         super(File);
+    }
+
+    query(userId: string, folderId: string, name: string, sort: string, order: ORDER, page: number, num: number): Promise<[File[], number]>{
+        const query = this.repository.createQueryBuilder();
+        query.andWhere('file.userId = :userId', { userId })
+        query.where('file.folderId = :folderId', { folderId });
+        if(name) query.andWhere('file.name LIKE :name', { name });
+        query.addOrderBy(sort, order)
+        return query.skip(num * ( page - 1 ))
+            .take(num)
+            .getManyAndCount();    
     }
 }
