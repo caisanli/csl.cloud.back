ce2d602393b58c11d48125feaa71639201dd1bc4
CaisanLi <18081007254@sina.cn>
2020-11-22 22:59:21 +0800

    文件分类

---
+++ app/helpers/fileCategory.ts
@@ -0,0 +1,9 @@
+export default {
+    '1' : ['3FR','ARM','AVIF','BMP','CR2','CRW','CUR','DCM','DCR','DDS','ERF','EXR','FAX','FTS','G3','G4','GIF','GC','HDR','HEIC','HEIF','HRZ','ICO','IIQ','IPL','JBG','JBIG','JFI','JFIF','JIF','JNX','JP2','JPE','JPEG','JPG','JPS','K25','KDC','MAC','MAP','MEF','MNG','MRW','MTV','NEF','NRW','OEF','OTB','PAL','PALW','PAM','PBM','PCD','PCT','PCX','PDB','PEF','PES','PFM','PGM','PGX','PICON','PICT','PIX','PLASMA','PNG','PNM','PPM','PSD','PWP','RAF','RAS','RGB','RGBA','RGBO','RGF','RGF','RLA','RLE','RW2','SCT','SFW','SGI','SIX','SIXEL','SR2','SRF','SUN','SVG','TGA','TIFF','TIM','TM2','UYVY','VIFF','VIPS','VIPS','WBMP','WEBP','WMZ','WPG','X3F','XBM','XC','XCF','XPM','XV','XWD','YUV'],
+    '2': ['AFM','BIN','CFF','CID','DFONT','OTF','PFA','PFB','PS','PT3','SFD','T11','T42','TTF','UFO','WOFF'],
+    '3': ['ABW', 'AW', 'CSV', 'DBK', 'DJVU', 'DOC', 'DOCM', 'DOCX', 'DOT', 'DOTM', 'DOTX', 'HTML', 'KWD', 'ODT', 'OXPS', 'PDF', 'RTF', 'SXW', 'TXT', 'WPS', 'XLS', 'XLSX', 'XPS'],
+    '4': ['7Z', 'ACE', 'ALZ', 'ARC', 'ARJ', 'CAB', 'CPIO', 'DEB', 'JAR', 'LHA', 'RAR', 'RPM', 'TAR', 'TBZ2', 'TGZ', 'ZIP'],
+    '5': ['ZAW3', 'EPUB', 'FB2', 'LRF', 'MOBI', 'PDB', 'RB', 'SNB', 'TCR'],
+    '6': ['3G2', '3GP', 'AAF', 'ASF', 'AV1', 'AVCHD', 'AVI', 'CAVS', 'DIVX', 'DV', 'F4V', 'FLV', 'HEVC', 'M2TS', 'M2V', 'M4V', 'MJPEG', 'MKV', 'MOD', 'MOV', 'MP4', 'MPEG', 'MPEG-2', 'MPG', 'MTS', 'MXF', 'OGV', 'RM', 'RMVB', 'SWF', 'TOD', 'TS', 'VOB', 'WEBM', 'WMV', 'WTV', 'XVID'],
+    '7': ['8SVX', 'AAC', 'AC3', 'AIFF', 'AMB', 'AMR', 'APE', 'AU', 'AVR', 'CAF', 'CDDA', 'CVS', 'CVSD', 'CVU', 'DSS', 'DTS', 'DVMS', 'FAP', 'FLAC', 'FSSD', 'GSM', 'GSRT', 'HCOM', 'HTK', 'IMA', 'IRCAM', 'M4A', 'M4R', 'MAUD', 'MP2', 'MP3', 'NIST', 'OGA', 'OGG', 'OPUS', 'PAF', 'PRC', 'PVF', 'RA', 'SD2', 'SHN', 'SLN', 'SMP', 'SND', 'SNDR', 'SNDT', 'SOU', 'SPH', 'SPX', 'TAK', 'TTA', 'TXW', 'VMS', 'VOC', 'VOX', 'VQF', 'W64', 'WAV', 'WMA', 'WV', 'WVE', 'XA']
+}
