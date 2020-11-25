4a410831b917d2ebafd51fc85b99f74a22745dfc
caigx <caigx@zxjmail.com>
2020-11-25 17:45:18 +0800

    增加上传公共方法

--- app/helpers/upload.ts
+++ app/helpers/upload.ts
@@ -2,35 +2,37 @@
 import fs from 'fs';
 import { Request } from 'koa';
 import multer from 'koa-multer';
-import path, { resolve } from 'path';
+import path from 'path';
+import archiver from 'archiver';
 import { createFileHash } from '.';
 import FileType from 'file-type';
 import { FileChunkEntity } from 'app/entities/mongodb';
 import core from 'file-type/core';
 import fileCategory from './fileCategory';
 import mime from 'mime';
+import { File, GroupFile } from 'app/entities/mysql';
 // 生成上传目录
 const uploadPath = path.join(__dirname, '../../', 'uploads');
 
 // multer 上传配置
 export const fileUploadOptions = {
     storage: multer.diskStorage({
-        destination: (req: Request, file: any, cb: any) => { 
+        destination: (req: Request, file: any, cb: any) => {
             const body: FileChunkEntity = req.body;
             // 根据文件名称、大小、最后修改时间生成Hash值
             const hashVal = createFileHash(body.name, body.size, body.modifyDate);
             // 用hash值生成切片的目录地址
             const chunkPath = path.join(uploadPath, hashVal, '/');
             // 检查切片目录地址是否存在 不存在就新创建
-            if(!fs.existsSync(chunkPath)) 
+            if (!fs.existsSync(chunkPath))
                 fs.mkdirSync(chunkPath, { recursive: true })
             // 将切片文件放到切片目录
             cb(null, chunkPath)
         },
-        filename: (req: Request, file: any, cb: any) => { 
+        filename: (req: Request, file: any, cb: any) => {
             const body = req.body;
             // 根据切片顺序生成新切片文件名
-            cb(null, `${ body.chunk }_${ body.name }`)
+            cb(null, `${body.chunk}_${body.name}`)
         }
     }),
     limits: {
@@ -46,7 +48,7 @@ export const fileUploadOptions = {
 export function clearChunkDir(hash: string) {
     // 生成切片目录地址
     const chunkPath = path.join(uploadPath, hash, '/');
-    const fileNames: string [] = fs.readdirSync(chunkPath);
+    const fileNames: string[] = fs.readdirSync(chunkPath);
     fileNames.forEach((name: string) => {
         let chunkFilePath = path.join(uploadPath, hash, name);
         fs.unlinkSync(chunkFilePath);
@@ -125,12 +127,12 @@ export function getExtName(name: string): string {
  */
 export function getCategory(name: string) {
     let ext = getExtName(name);
-    if(!ext) return '0';
+    if (!ext) return '0';
     const category = fileCategory;
     ext = ext.toUpperCase();
     for (const key in category) {
-        let val = category[key].find( (val: string) => val === ext)
-        if(val) return key;
+        let val = category[key].find((val: string) => val === ext)
+        if (val) return key;
     }
     return '0';
 }
@@ -202,7 +204,7 @@ export function getFileBuffer(relationPath: string, opt: any): Promise<Buffer> {
         let arr = [], len = 0;
         fileStream.on('data', chunk => {
             len += chunk.length;
-            arr.push( Buffer.from(chunk));
+            arr.push(Buffer.from(chunk));
         });
         fileStream.on('end', () => {
             resolve(Buffer.concat(arr, len));
@@ -210,5 +212,71 @@ export function getFileBuffer(relationPath: string, opt: any): Promise<Buffer> {
         fileStream.on('error', err => {
             reject(err)
         })
+    })
+}
+
+/**
+ * 批量打包文件
+ * @export
+ * @param {string[]} fileIds
+ * @returns {Promise<Buffer>}
+ */
+export function zip(files: File[] | GroupFile[], basePath?: string): Promise<Buffer> {
+    return new Promise((resolve) => {
+        const newPath = './' + Date.now() + '.zip';
+        // 生成压缩包地址
+        const zipPath = path.join(uploadPath, newPath);
+        // 生成压缩包的可写流
+        const output = fs.createWriteStream(zipPath);
+        const archive = archiver('zip', {
+            zlib: { level: 9 } // 设置压缩级别
+        });
+        // listen for all archive data to be written
+        // 'close' event is fired only when a file descriptor is involved
+        output.on('close', async function () {
+            console.log(archive.pointer() + ' total bytes');
+            console.log('archiver has been finalized and the output file descriptor has closed.');
+            const buffer = await getFileBuffer(newPath, null);
+            removeFile(newPath)
+            resolve(buffer);
+        });
+
+        // This event is fired when the data source is drained no matter what was the data source.
+        // It is not part of this library but rather from the NodeJS Stream API.
+        // @see: https://nodejs.org/api/stream.html#stream_event_end
+        output.on('end', function () {
+            console.log('Data has been drained');
+        });
+
+        // good practice to catch warnings (ie stat failures and other non-blocking errors)
+        archive.on('warning', function (err) {
+            if (err.code === 'ENOENT') {
+                // log warning
+            } else {
+                // throw error
+                throw err;
+            }
+        });
+
+        // good practice to catch this error explicitly
+        archive.on('error', function (err) {
+            throw err;
+        });
+
+        // pipe archive data to the file
+        archive.pipe(output);
+        // 依次加入文件
+        files.forEach((file: File | GroupFile) => {
+            let filePath: string = path.join(uploadPath);
+            if(basePath) 
+                filePath = path.join(filePath, basePath);
+            filePath = path.join(filePath, './' + file.id);
+            try {
+                archive.file(filePath, { name: file.name });
+            } catch (error) {
+                
+            }
+        })
+        archive.finalize();
     })
 }
