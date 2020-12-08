d0071e245377ea44de64076ecd4dc361b324d609
CaisanLi <18081007254@sina.cn>
2020-12-08 22:07:42 +0800

    修改不能按名称模糊查询

--- app/services/file.service.ts
+++ app/services/file.service.ts
@@ -12,7 +12,7 @@ export class FilService extends BaseService<File> {
         const query = this.repository.createQueryBuilder();
         query.andWhere('file.userId = :userId', { userId })
         query.where('file.folderId = :folderId', { folderId });
-        if(name) query.andWhere('file.name LIKE :name', { name });
+        if(name) query.andWhere('file.name LIKE :name', { name: `%${ name }%` });
         query.addOrderBy(sort, order)
         return query.skip(num * ( page - 1 ))
             .take(num)
