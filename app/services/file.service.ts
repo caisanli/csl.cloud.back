095f2ec0bd0822a00b5975456866c8ea7f53c5db
CaisanLi <18081007254@sina.cn>
2020-12-11 22:09:32 +0800

    增加分类查询

--- app/services/file.service.ts
+++ app/services/file.service.ts
@@ -1,5 +1,5 @@
 import { File } from "app/entities/mysql";
-import { ORDER } from "app/typings";
+import { CATEGORY, ORDER } from "app/typings";
 import { Service } from "typedi";
 import { BaseService } from "./base";
 @Service()
@@ -7,17 +7,36 @@ export class FilService extends BaseService<File> {
     constructor() {
         super(File);
     }
-
-    query(userId: string, folderId: string, name: string, sort: string, order: ORDER, page: number, num: number): Promise<[File[], number]>{
+    /**
+     * 多条件查询
+     * @param {string} userId
+     * @param {string} name
+     * @param {string} sort
+     * @param {ORDER} order
+     * @param {number} page
+     * @param {number} num
+     * @param {string} [folderId]
+     * @param {CATEGORY} [category]
+     * @returns {Promise<[File[], number]>}
+     * @memberof FilService
+     */
+    query(userId: string, name: string, sort: string, order: ORDER, page: number, num: number, folderId?: string, category?: CATEGORY): Promise<[File[], number]>{
         const query = this.repository.createQueryBuilder();
-        query.andWhere('file.userId = :userId', { userId })
-        query.where('file.folderId = :folderId', { folderId });
+        query.where('file.userId = :userId', { userId })
+        if(folderId) {
+            query.where('file.folderId = :folderId', { folderId });
+        }  
+        if(category) {
+            query.where('file.category = :category', { category });
+        }
+        
         if(name) query.andWhere('file.name LIKE :name', { name: `%${ name }%` });
         query.addOrderBy(sort, order)
         return query.skip(num * ( page - 1 ))
             .take(num)
             .getManyAndCount();    
     }
+
     
     getFileCount(folderId: string): Promise<number> {
         return this.repository.createQueryBuilder()
