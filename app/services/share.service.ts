295a5eea82b9706525310e5d782d99bd2f305a44
CaisanLi <18081007254@sina.cn>
2020-12-13 22:12:22 +0800

    增加新service

--- app/services/share.service.ts
+++ app/services/share.service.ts
@@ -5,4 +5,24 @@ export default class ShareService extends BaseService<Share> {
     constructor() {
         super(Share);
     }
+    getByUser(userId: string):Promise<Share[]> {
+        return this.repository.createQueryBuilder('share')
+                    // .leftJoinAndSelect('share.files', 'file', 'file.category = :category', { category: '4' })
+                    .where('share.userId = :userId', { userId })
+                    .getMany();
+    }
+
+    getToMeByUser(userId: string):Promise<Share[]> {
+        return this.repository.createQueryBuilder('share')
+                .innerJoinAndSelect('share.users', 'user', 'user.id = :userId', { userId })
+                .getMany();
+    }
+
+    getDetail(id: string): Promise<Share> {
+        return this.repository.createQueryBuilder('share')
+                .leftJoinAndSelect('share.files', 'file')
+                .leftJoinAndSelect('share.folders', 'folder')
+                .where('share.id = :id', { id })
+                .getOne();
+    }
 }
