dc81fb0b9b93e4be3fa75f0ae3ee592b954a679d
蔡三李 <caisanli_0105@163.com>
2023-08-26 11:13:22 +0800

    chore(*)：更新数据连接地址

--- configs/environments/development.ts
+++ configs/environments/development.ts
@@ -4,14 +4,14 @@ export default {
   port: 3000,
 
   MG_DB_TYPE: 'mongodb',
-  MG_MY_HOST: '127.0.0.1',
+  MG_MY_HOST: 'localhost',
   MG_MY_PORT: 27017,
   MG_USERNAME: 'root',
   MG_PASSWORD: '962464',
   MG_DATABASE: 'cloud',
 
   MQ_DB_TYPE: 'mysql',
-  MQ_MY_HOST: '127.0.0.1',
+  MQ_MY_HOST: 'localhost',
   MQ_MY_PORT: 3306,
   MQ_USERNAME: 'root',
   MQ_PASSWORD: 'Caisan,0105',
